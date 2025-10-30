b37c67403402af0c2ac20f20d57e5d227445939a
Tushar Laad <tusharlaad2002@gmail.com>
2025-10-30 12:02:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3845013162-4f4cd",
+  "appId": "1:809765872395:web:f36d801e758c77ee3eb80e",
+  "apiKey": "AIzaSyDOL79smWOUYPwNlzPtVVioPbHFX04_yhU",
+  "authDomain": "studio-3845013162-4f4cd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "809765872395"
+};
